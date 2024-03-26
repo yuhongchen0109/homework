cd904b2992b0f62d7de9d4f85589700d775705e8
yuhongchen0109 <163223187+yuhongchen0109@users.noreply.github.com>
2024-03-26 18:03:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,5 @@
 from application.application import app
 
-from flask import Flask, render_template, request, redirect, url_for
-import openai
-import os
 
-app = Flask(__name__)
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-@app.route('/')
-def index():
-    return render_template('home.html')
-
-@app.route('/generate', methods=['POST'])
-def generate():
-    prompt = request.form['prompt']
-    response = openai.ChatCompletion.create(
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-        model="gpt-3.5-turbo-0125",
-        temperature = 0.5,
-    )
-    generated_text = response['choices'][0]['message']['content'].strip()
-    return render_template('index.html', response=generated_text)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
